294fd5d7e068597387deb11346d813f7b687d08e
Jonathan Ong <jonathanrichardong@gmail.com>
2014-06-28 16:19:08 +0100

    clarify to use response.set()

--- 06-content-negotiation/index.js
+++ 06-content-negotiation/index.js
@@ -5,7 +5,10 @@ var koa = require('koa');
  * This is a promisified version of zlib's gzip function,
  * allowing you to simply `yield` it without "thunkifying" it.
  *
- *   this.response.body = yield gzip('something');
+ *   app.use(function* (next) {
+ *     this.response.set('Content-Encoding', 'gzip');
+ *     this.response.body = yield gzip('something');
+ *   })
  */
 var gzip = require('mz/zlib').gzip;
 
